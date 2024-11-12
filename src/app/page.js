ca1fff82b18a6d965dbe307cf1333edf6faff487
Achraf Mehanneche <107757609+DigitalDeveloperAM@users.noreply.github.com>
2024-11-11 22:34:12 -0800

    The editor is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,53 @@
 "use client";
-import TextEditor from '@/components/TextEditor';
 import React from 'react';
+import TextEditor from '@/components/TextEditor';
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 
 function page() {
+  const form = useForm({
+    mode: "onTouched",
+    defaultValues: {
+      post: "",
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <TextEditor></TextEditor>
+    <div className="max-w-3xl mx-auto py-5">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="post"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Post</FormLabel>
+                <FormControl>
+                  <TextEditor
+                    content={field.value}
+                    onChange={(value) => field.onChange(value)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="mt-4">Save</Button>
+        </form>
+      </Form>
     </div>
   )
 }
--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -4,17 +4,51 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Heading from '@tiptap/extension-heading';
 import TextAlign from '@tiptap/extension-text-align';
-import Highlight from "@tiptap/extension-Highlight";
-import Image from "@tiptap/extension-Image";
+import Highlight from '@tiptap/extension-highlight';
+import Image from '@tiptap/extension-image';
 import BulletList from "@tiptap/extension-bullet-list";
 import OrderList from "@tiptap/extension-ordered-list";
 import ToolBar from './ToolBar';
 
-function TextEditor() {
+function TextEditor({content, onChange}) {
+ const editor = useEditor({
+    extensions: [
+        StarterKit.configure(),
+        TextAlign.configure({
+            types: ["heading", "paragraph"],
+        }),
+        Heading.configure({
+            levels: [1, 2, 3],
+        }),
+        OrderList.configure({
+            HTMLAttributes: {
+                class: "list-decimal ml-3",
+            }
+        }),
+        BulletList.configure({
+            HTMLAttributes: {
+                class: "list-disc ml-3",
+            },
+        }),
+        Highlight,
+        Image,
+    ],
+    content,
+    onUpdate: ({editor}) => {
+        console.log(editor.getHTML());
+        onChange(editor.getHTML());
+    },
+    editorProps: {
+        attributes:{
+            class:"min-h-[156px] border rounded-md bg-slate-50 py-2 px-3",
+        }
+    }
+ })
+
   return (
     <div>
-      <ToolBar></ToolBar>
-      <EditorContent></EditorContent>
+      <ToolBar editor={editor}></ToolBar>
+      <EditorContent editor={editor}></EditorContent>
     </div>
   )
 }
